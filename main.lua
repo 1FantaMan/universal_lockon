2fca5ced3562e405a6d58f994770a3e5fe7b2653
FantaMan <turtleman040@gmail.com>
2025-12-10 11:22:10 +0300

    added outputs

--- main.lua
+++ main.lua
@@ -3,12 +3,15 @@ local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 
 --// Variables
+local Configs = {
+    IsActive = true,
+    VelocityPrediction = false,
+}
+
 local Target = {
     Position = Vector3.new(),
     RootPart = nil :: BasePart
 }
-
-local IsActive = true
 
 --// Functions
 local function InputBegan(input: InputObject, gameProcessedEvent: boolean)
@@ -17,3 +20,5 @@ end
 
 --// Signals
 UserInputService.InputBegan:Connect(InputBegan)
+
+print("Universal Lock_on Loaded Successfully!")
